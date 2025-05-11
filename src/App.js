9d71fb8f3ad0d07dc00c770821d67c07539bb64b
shanmukhabhiram <shanmukhabhiram5@gmail.com>
2025-05-11 20:36:08 +0530

    Implement routing for Portfolio and AdminViews components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 
 function App() {
@@ -17,7 +18,13 @@ function App() {
         <Experience />
         <Contact />
       </div> */}\
-      <Portfolio />
+      {/* <Portfolio /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/admin-views" element={<AdminViews />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
